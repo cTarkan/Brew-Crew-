2f1a97b24a4a74736bcc00fff8bde9a5022ce6eb
Cenkhan TARKAN <cenkhantarkan@hotmail.com>
2019-12-23 23:28:04 +0300

    anonymous user login has done

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,20 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user obj based on FirebaseUser
+  User _userFromFilebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
 
   //sign in anonymously
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFilebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
